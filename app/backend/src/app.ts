353825aa1577cb377d7d9fb4cb56a7ac70b1ce22
Guilherme <guilhermehallmann@gmail.com>
2022-07-06 14:37:40 -0300

    initial commit

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -25,7 +25,7 @@ class App {
   }
 
   public start(PORT: string | number):void {
-    this.app.listen(PORT, () => console.log(`Running on port ${PORT}`));
+    this.app.listen(PORT, () => console.log(`Rodando na porta ${PORT}`));
   }
 }
 
