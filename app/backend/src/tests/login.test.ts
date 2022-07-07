c7a915ed9b6f418782c7ae8b34d1be58295076af
Guilherme <guilhermehallmann@gmail.com>
2022-07-07 19:08:50 -0300

    feat: creating a test for a successful login

---
+++ app/backend/src/tests/login.test.ts
@@ -0,0 +1,49 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+import User from '../database/models/user';
+
+import { Response } from 'superagent';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Testing a successful login', () => {
+  let chaiHttpResponse: Response;
+
+  before(async () => {
+    sinon
+      .stub(User, "findOne")
+      .resolves({
+        email: 'admin@admin.com',
+        password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW'
+      } as User);
+  });
+
+  after(()=>{
+    (User.findOne as sinon.SinonStub).restore();
+  })
+
+  it('Should return a status code 200 if the request was successful', async () => {
+    chaiHttpResponse = await chai
+       .request(app)
+       .post('/login')
+       .send({email: 'admin@admin.com', password: 'secret_admin' })
+
+    expect(chaiHttpResponse).to.have.status(200);
+  });
+
+  it('Should return a token if the request was successful', async () => {
+    chaiHttpResponse = await chai
+       .request(app)
+       .post('/login')
+       .send({email: 'admin@admin.com', password: 'secret_admin' })
+
+    expect(chaiHttpResponse.body).to.have.property('token');
+    expect(chaiHttpResponse.body.token).to.be.a('string');
+  });
+});
