316d6e25e3ec87bd633c6a59e91bb54c4c9177bb
Guilherme <guilhermehallmann@gmail.com>
2022-07-11 16:43:34 -0300

    feat: adding more tests to login function

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -47,3 +47,38 @@ describe('Testing a successful login', () => {
     expect(chaiHttpResponse.body.token).to.be.a('string');
   });
 });
+
+describe('Testing a unsuccessful login', () => {
+  let chaiHttpResponse: Response;
+
+  before(async () => {
+    sinon
+      .stub(User, "findOne")
+      .resolves({
+        email: 'admin@admin.com',
+        password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW'
+      } as User);
+  });
+
+  after(()=>{
+    (User.findOne as sinon.SinonStub).restore();
+  })
+
+  it('Should return a status code 400 if the email is invalid', async () => {
+    chaiHttpResponse = await chai
+       .request(app)
+       .post('/login')
+       .send({email: 'admin.com', password: 'secret_admin' })
+
+    expect(chaiHttpResponse).to.have.status(400);
+  });
+
+  it('Should return a status code 400 if the password is invalid', async () => {
+    chaiHttpResponse = await chai
+       .request(app)
+       .post('/login')
+       .send({email: 'admin@admin.com', password: '111' })
+
+    expect(chaiHttpResponse).to.have.status(400);
+  });
+});
